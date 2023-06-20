8884383129d1221f8b33f679b845d886ed0b9f12
Jake Bogan <jakebogan01@gmail.com>
2023-06-20 13:28:30 -0400

    update vite config file frontend

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
-		    '/api': 'http://localhost:4000',
+		    '/api': 'https://backend-workout.vercel.app',
 		},
 	 },
 });
